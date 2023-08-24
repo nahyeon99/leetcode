129008fae6583139f3d5762469e405d0a1ca8ac6
nahyeon99 <nahyeonee99@gmail.com>
2023-08-24 20:11:28 +0900

    [leetcode] 80. Remove Duplicates from Sorted Array II

---
+++ src/RemoveDuplicatesFromSortedArray2/Solution.java
@@ -0,0 +1,22 @@
+package RemoveDuplicatesFromSortedArray2;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int lastValue = 100001;
+        boolean checkTwice = false;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != lastValue) {
+                lastValue = nums[i];
+                checkTwice = false;
+                nums[index] = nums[i];
+                index++;
+            } else if(!checkTwice) {
+                nums[index] = nums[i];
+                index++;
+                checkTwice = true;
+            }
+        }
+        return index;
+    }
+}
