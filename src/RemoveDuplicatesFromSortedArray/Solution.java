854a0cdc8d98d0d85975e145e0bdba993eb5960d
nahyeon99 <nahyeonee99@gmail.com>
2023-08-24 19:57:19 +0900

    [leetcode] 26. Remove Duplicates from Sorted Array

---
+++ src/RemoveDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,16 @@
+package RemoveDuplicatesFromSortedArray;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int lastValue = -101;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != lastValue) {
+                nums[index] = nums[i];
+                lastValue = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
