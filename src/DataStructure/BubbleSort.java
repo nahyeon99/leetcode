9b615353371c7d3b3bc9454866384e32154daaea
nahyeon99 <nahyeonee99@gmail.com>
2023-09-05 13:11:19 +0900

    [DataStructure] bubble sort

---
+++ src/DataStructure/BubbleSort.java
@@ -0,0 +1,31 @@
+package DataStructure;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BubbleSort {
+
+    public List<Integer> bubbleSort(ArrayList<Integer> list) {
+
+        for (int i = list.size() - 1 ; i >= 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+                if (list.get(j) > list.get(j + 1))
+                    Collections.swap(list, j, j + 1);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add((int)(Math.random() * 100));
+        }
+        System.out.println(list);
+
+        BubbleSort bs = new BubbleSort();
+        System.out.println(bs.bubbleSort(list));
+    }
+}
